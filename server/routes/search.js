d4bb723b13413d9d5cb0d9948279aa47bae59374
Isaugum <gorjup.domen95@gmail.com>
2023-01-23 00:02:20 +0100

    NeDB import fixed

--- server/routes/search.js
+++ server/routes/search.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const database = require("../loadDatabase");	
+const { database } = require("../loadDatabase");	
 let router = express.Router();
 
 router.get("/", (req, res) => {
